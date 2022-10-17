1c5dde8b63b1368467f0b0bf53d9df54bff2096c
Erick <erickmendesweb@gmail.com>
2022-10-17 17:58:03 -0300

    Projeto FINALIZADO

--- script.js
+++ script.js
@@ -83,23 +83,38 @@ function enviarmensagem() {
     let classebebidas = document.querySelector(".selectionDrink")
     let classesobremesas = document.querySelector(".selectionDessert")
 
+    // Parte do Prato //
+
     let childrenpratos = classepratos.children
     let nomeprato = childrenpratos[1].textContent
-    // let precoprato = childrenpratos[3].classList("price")
-    console.log(childrenpratos[3])
+    nomeprato = nomeprato.trim()
+    let preco1 = Number(parseFloat(childrenpratos[3].querySelector("span.price").textContent).toFixed(2))
+    
+
+
+    // Parte da Bebiba //
 
     let childrenbebidas = classebebidas.children
     let nomebebida = childrenbebidas[1].textContent
+    nomebebida = nomebebida.trim()
+    let preco2 = Number(parseFloat(childrenbebidas[3].querySelector("span.price").textContent).toFixed(2))
+    
+
+    // Parte da Sobremesa //
 
     let childrensobremesas = classesobremesas.children
     let nomesobremesa = childrensobremesas[1].textContent
+    nomesobremesa = nomesobremesa.trim()
+    let preco3 = Number(parseFloat(childrensobremesas[3].querySelector("span.price").textContent).toFixed(2))
+    
+    // Preco final da compra //
+    let precofinal = parseFloat(preco1 + preco2 + preco3).toFixed(2)
+    
 
-    let precofinal
-
-
+    // Parte do link pra mensagem //
     let celular = "5524988437290";
 
-    let texto = `Olá, gostaria de fazer o pedido: \n- Prato: ${nomeprato} \n- Bebida: ${nomebebida} \n- Sobremesa: ${nomesobremesa} \nTotal: R$ 27.70`
+    let texto = `Olá, gostaria de fazer o pedido: \n- Prato:${nomeprato}\n- Bebida:${nomebebida} \n- Sobremesa:${nomesobremesa} \nTotal: R$${precofinal}`
 
     texto = window.encodeURIComponent(texto)
 
