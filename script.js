61746092852544b8c5294e53695a61282faa556b
Erick <erickmendesweb@gmail.com>
2022-10-17 15:56:40 -0300

    Layout botao fechar pedido finalizado

--- script.js
+++ script.js
@@ -1,20 +1,45 @@
+let prato = false;
+let bebida = false;
+let sobremesa = false;
+
 
 function greenButton() {
-    
-    const main = document.querySelector(".foodContainer")
-    const contador = 0;
 
-if(main.classList("selectionFood") !==null && main.classList.contains("selectionDrink") !==null && main.classList.contains("selectionDessert") !==null ){
-    contador = 3;
+    let botao = document.querySelector(".button")
+
+    let classesprato = document.querySelector(".prato")
+    let classesbebida = document.querySelector(".bebida")
+    let classessobremesa = document.querySelector(".sobremesa")
+
+    // A children de cada opção //
+    let childrenprato = classesprato.children;
+    let childrenbebida = classesbebida.children;
+    let childrensobremesa = classessobremesa.children;
+
+    for (let i = 0; i < childrenprato.length; i++) {
+        if (childrenprato[i].classList.contains("selectionFood")){
+            prato = true
+        }
+    }
+
+    for (let i = 0; i < childrenbebida.length; i++) {
+        if (childrenbebida[i].classList.contains("selectionDrink")){
+            bebida = true
+        }
+    }
+
+    for (let i = 0; i < childrensobremesa.length; i++) {
+        if (childrensobremesa[i].classList.contains("selectionDessert")){
+            sobremesa = true
+        }
+    }
+
+    if(prato && bebida && sobremesa){
+        botao.classList.add("green")
+        botao.innerHTML = "Fechar pedido"
+    }
     
 }
-    const botao = document.querySelector(".button")
-            if(contador == 3){    
-                botao.classList.add("green")
-                botao.innerHTML = "Fechar Pedido"
-            }
-}
-
 function addClassSelectionFood(element) {
     const selected = document.querySelector(".selectionFood")
     if( selected !== null) {
@@ -23,6 +48,8 @@ function addClassSelectionFood(element) {
     element.classList.toggle("selectionFood")
 
     greenButton();
+
+    
     
 }
 
